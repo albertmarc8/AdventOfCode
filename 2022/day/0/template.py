3e5a15385e976f4de398c9ee3f92de51c4efb520
Albert Marc <albertmarc8@gmail.com>
2022-12-22 01:52:56 +0100

    Updated template with time

--- 2022/day/0/template.py
+++ 2022/day/0/template.py
@@ -1,17 +1,23 @@
 import sys
+import time
 from typing import *
 
 
 def read_data(f: IO):
     pass
 
+
 def process(c: List[List[int]]):
     pass
+
 
 def show_results(n_elf: List[int], calories_carrying: List[int]):
     pass
 
+
 if __name__ == '__main__':
+    start_time = time.time()
     c = read_data(sys.stdin)
     n, n_cal = process(c)
     show_results(n, n_cal)
+    print(f"--- %.8s seconds ---" % (time.time() - start_time))
